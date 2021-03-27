5bc222c7f1c1f3b5a65792ab1a90a48a443e4897
ABC10946 <alphalex10946@gmail.com>
2021-03-27 16:44:04 +0900

    ウィンドウを拡大

--- gui.h
+++ gui.h
@@ -2,8 +2,8 @@
 #define GUI_H
 #include <GL/glut.h>
 #include "tetris.h"
-#define WINDOW_WIDTH 340
-#define WINDOW_HEIGHT 520
+#define WINDOW_WIDTH 520
+#define WINDOW_HEIGHT 560
 #define FRAME_FREQ ((1.0/60.0) * 1000)
 
 void display(void);
